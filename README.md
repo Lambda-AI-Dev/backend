7435ee5231ac185eb527382676015a6484ebf3e1
Daniel Tian <dtian.95@gmail.com>
2020-02-21 11:24:58 -0500

    setup .env file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# backend
+# Lambda AI Backend
+
 The backend functions on AWS Lambda.
+
+# Get Started
+
+- Created .env file in root directory
+- .env file follows the following structure:
+  - REGION=XXXXX
+  - ACCESS_ID=XXXXX
+  - SECRET_KEY=XXXXX
--- callAwsFunction.js
+++ callAwsFunction.js
@@ -1,51 +1,56 @@
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
+require("dotenv").config();
 
 // AWS config details (TODO: Should be hidden in a credentials file)
-// TODO: Replace with AWS config details
+AWS.config.update({
+  region: process.env.REGION,
+  accessKeyId: process.env.ACCESS_ID,
+  secretAccessKey: process.env.SECRET_KEY
+});
 
 /**
  * Invoke a AWS Lambda function with particular name and inputs
- * 
+ *
  * @param functionName The AWS Lambda function name
  * @param input The inputs you want to pass into the AWS Lambda function
  * @param callback The callback function returning the response from the AWS Lambda Function
  */
-function callAwsLambda (functionName, input, callback) {
-    // Create Lambda object
-    let lambda = new AWS.Lambda();
-  
-    // Create the Lambda function input parameters
-    let params = { 
-      FunctionName: functionName, 
-      InvocationType: "RequestResponse", 
-      LogType: "Tail",
-      Payload: JSON.stringify(input)
-    };
-  
-    // Invoke the Lambda function
-    lambda.invoke(params, function(err, data) {
-        callback(err, data);
-    });
+function callAwsLambda(functionName, input, callback) {
+  // Create Lambda object
+  let lambda = new AWS.Lambda();
+
+  // Create the Lambda function input parameters
+  let params = {
+    FunctionName: functionName,
+    InvocationType: "RequestResponse",
+    LogType: "Tail",
+    Payload: JSON.stringify(input)
+  };
+
+  // Invoke the Lambda function
+  lambda.invoke(params, function(err, data) {
+    callback(err, data);
+  });
 }
 
 /**
  * Get a Multiple Choice image set to be labeled
- * 
+ *
  * @param labelerId The cookie fetched from the labeler
  */
 function getMultipleChoiceImage(labelerId) {
-    var input = {
-        labelerId: labelerId
-    };
-    callAwsLambda("getMultipleChoiceImage", input, (err, data) => {
-        if (err) {
-            console.error('There was an error in invoking the function. See below.');
-            console.error(err);
-        } else {
-            console.log('The data was successfully returned; see below.');
-            console.log(data);
-        }
-    });
+  var input = {
+    labelerId: labelerId
+  };
+  callAwsLambda("getMultipleChoiceImage", input, (err, data) => {
+    if (err) {
+      console.error("There was an error in invoking the function. See below.");
+      console.error(err);
+    } else {
+      console.log("The data was successfully returned; see below.");
+      console.log(data);
+    }
+  });
 }
 
 // No labelerId passed in, function will return with written warning.
