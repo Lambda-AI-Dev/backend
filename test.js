75d5a24d0ee6aeea29f36593886b765538ef72b3
Chris Elliott <elliott77chris@gmail.com>
2020-03-20 21:46:43 -0700

    Completed the getBatchMultipleChoiceTask file
    I completed and tested an early implementation of 'getBatchMultipleChoiceTask', checking only in the test.js file

--- test.js
+++ test.js
@@ -17,6 +17,10 @@ let taskCount = 2;
 let labelerId = '6snPx3hh1MYgnaha';
 let type = 'text';
 
+// Define constants
+const maxClasses = 5; // Number of classes to be given in testing
+const maxOccurrences = 5; // Number of times a class should be given in a task
+
 // Scan 'labeler_task' table for tasks of 'type' already completed by 'labelerId'
 let params = {
   TableName: 'labeler_task',
@@ -31,57 +35,94 @@ let params = {
   }
 }
 
-// TODO: Address 'lastEvaluatedKey', if needed
+// NOTE: If 'labelerId' has more than 100 tasks currently in database associated with his
+// profile, it will not get the rest
+
+// TODO: Clean up tasks associated with a 'labelerId'
 
 // Scan 'labeler_task' table
-docClient.scan(params, (error, data) => {
-  if (!error) {
-    // Define 'taskObject' to filter results to not contain already-given tasks
-    let taskObject = {};
-    let index = 0;
-    data.Items.forEach((item) => {
-      console.log(item);
-      taskObject[(':taskId' + index)] = item.taskId;
-      index++;
-    });
-
-    // Scan 'task' table for tasks of 'type' and not previously given to 'labelerId'
-    let params = {
-      TableName: 'task',
-      Limit: taskCount,
-      FilterExpression: 'NOT ( #taskId IN (' + Object.keys(taskObject).toString()
-        + ') ) AND #type = :type AND #progress < 1',
-      ExpressionAttributeNames: {
-        '#progress': 'progress',
-        '#taskId': 'taskId',
-        '#type': 'type'
-      },
-      ProjectionExpression: 'instructions, datasetId, taskId, multiclass, '
-    }
-
-    // Assign value mappings for FilterExpression
-    taskObject[':type'] = type;
-    params.ExpressionAttributeValues = taskObject;
-
-    // Scan 'task' table
-    docClient.scan(params, (error, data) => {
-      if (!error) {
-        console.log(data);
-        // Construct return object
-        let tasks = [];
-        
-        // Iterate through available tasks
-        data.Items.forEach((item) => {
-          let task = {};
-          task.instructions = item.instructions;
-          task.datasetId = item.datasetId;
-        });
-      } else {
-        console.error(error);
+let promise = new Promise((resolve, reject) => {
+  docClient.scan(params, (error, data) => {
+    if (!error) {
+      // Define 'taskObject' to filter results to not contain already-given tasks
+      let taskObject = {};
+      let index = 0;
+      data.Items.forEach((item) => {
+        taskObject[(':taskId' + index)] = item.taskId;
+        index++;
+      });
+  
+      // Scan 'task' table for tasks of 'type' and not previously given to 'labelerId'
+      let params = {
+        TableName: 'task',
+        Limit: taskCount,
+        FilterExpression: 'NOT ( #taskId IN (' + Object.keys(taskObject).toString()
+          + ') ) AND #type = :type AND #progress < :one',
+        ProjectionExpression: 'instructions, datasetId, taskId, multiclass, #data, #class, #type',
+        ExpressionAttributeNames: {
+          '#progress': 'progress',
+          '#taskId': 'taskId',
+          '#type': 'type',
+          '#data': 'data',
+          '#class': 'class'
+        }
       }
-    });
+  
+      // Assign value mappings for FilterExpression
+      taskObject[':type'] = type;
+      taskObject[':one'] = 1;
+      params.ExpressionAttributeValues = taskObject;
+  
+      // Scan 'task' table
+      docClient.scan(params, (error, data) => {
+        if (!error) {
+          // Construct return object
+          let tasks = [];
+          
+          // Iterate through available tasks
+          data.Items.forEach((item) => {
+            let index = 0;
+            let total = Object.entries(item.class).length;
+  
+            let classes = {};
+            Object.keys(item.class).forEach((className, occurrences) => {
+              // Automatically add class if there are not enough remaining
+              // OW, add if class needs to be given out more
+              if ((total - index - 1) + Object.entries(classes).length <= maxClasses) {
+                classes[className] = false;
+              } else if (occurrences < maxOccurrences) {
+                classes[className] = false;
+              }
+            });
+  
+            // Replace current 'class' variable with the classes to be tested
+            item.class = classes;
+  
+            // Set other variables specific to the task
+            item.labelingMethod = 'multipleChoice';
+            item.stoppedByTimer = null;
+            item.beginTimestamp = null;
+            item.endTimestamp = null;
+  
+            // Add item to 'tasks', to be returned
+            tasks.push(item);
+          });
+  
+          resolve(tasks);
+        } else {
+          error.note = 'The scan operation for the \'task\' table failed.';
+          reject(error);
+        }
+      });
+    } else {
+      error.note = 'The scan operation for the \'labeler_task\' table failed.';
+      reject(error);
+    }
+  });
+});
 
-  } else {
-    console.log(err);
-  }
-})
+promise.then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log(error);
+});
